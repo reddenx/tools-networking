aea672d1f6986827d680b22ac22c35a0e5f116a1
SM_D <seanmarek@gmail.com>
2015-02-21 10:20:06 -0800

    low level keyboard eventer working

---
+++ App.L33T3R1Zr/Form1.cs
@@ -0,0 +1,34 @@
+﻿using SMT.Utilities.InputEvents.HardwareEvents;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App.L33T3R1Zr
+{
+    public partial class Form1 : Form
+    {
+        private KeyboardEventListener KeyboardListener;
+
+        public Form1(KeyboardEventListener keyboardListener)
+        {
+            KeyboardListener = keyboardListener;
+
+            InitializeComponent();
+
+            this.textBox1.Enabled = false;
+
+            KeyboardListener.SetHook(PrependText);
+        }
+
+        private void PrependText(string msg)
+        {
+            this.textBox1.Text = msg + "\r\n" + this.textBox1.Text;
+        }
+    }
+}
