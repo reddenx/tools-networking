919052cf2221d48390c5a34f0273667ecae4761e
sean-surf <seanmarek@gmail.com>
2015-04-01 22:39:29 -0700

    figuring out xml parsing

---
+++ SMT.Proxy.FogBugz/FogBugzObjects/Case.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SMT.Proxy.FogBugz.FogBugzObjects
+{
+    [XmlRootAttribute("case")]
+    public class Case
+    {
+        [XmlElement("ixBug")]
+        public int Id;
+        
+        [XmlElement("ixBugParent")]
+        public int? Parent;
+
+        [XmlElement("ixBugChildren")]
+        public string _children;
+        public int[] Children { get { return _children.Split(',').Select(c => int.Parse(c)).ToArray(); } }
+
+        [XmlElement("tags")]
+        public Tags _tags;
+        public string[] Tags { get { return _tags.Tag; } }
+
+
+    }
+}
