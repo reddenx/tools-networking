c429197458b18f5cfaf6ddcd5c8b5ecc86b4c79d
SM_D <seanmarek@gmail.com>
2015-04-16 21:22:48 -0700

    added phone remote winphone app

---
+++ App.Phone.PhoneRemote/ViewModels/ViewModelBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Phone.PhoneRemote.ViewModels
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        //tried something too fancy... forgot how to reflect XD
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpresssion)
+        {
+            string propertyName = ((propertyExpresssion.Body as MemberExpression).Member as PropertyInfo).Name;
+
+            this.OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            this.OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
