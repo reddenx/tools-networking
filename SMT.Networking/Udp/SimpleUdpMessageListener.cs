58707ae91ced037827fa6856ccb5ecaf34e1f63c
sean-surf <seanmarek@gmail.com>
2015-05-09 12:54:42 -0700

    udp reworking

---
+++ SMT.Networking/Udp/SimpleUdpMessageListener.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SMT.Networking.Udp
+{
+    class SimpleUdpMessageListener<TMessage>
+    {
+        public event EventHandler<TMessage> OnMessageReceived;
+        public event EventHandler<string> OnNetworkError;
+        private UdpClient Client;
+        private readonly BinaryFormatter Formatter;
+        private int ListenPort;
+        private Thread ListenThread;
+
+        public SimpleUdpMessageListener()
+        {
+            Client = null;
+            Formatter = new BinaryFormatter();
+        }
+
+        public void StartListening(int port)
+        {
+            if (Client != null)
+            {
+                CleanupClient();
+            }
+
+            try
+            {
+                Client = new UdpClient();
+                ListenPort = port;
+                //Client.Client.Bind(new IPEndPoint(IPAddress.Any, port)); //may not be necessary?
+                StartListenLoop();
+            }
+            catch (SocketException e)
+            {
+                RaiseError(e.Message);
+            }
+        }
+
+        private void StartListenLoop()
+        {
+            if (ListenThread != null)
+            {
+                CleanupThread();
+            }
+
+            ListenThread = new Thread(new ThreadStart(ListenLoop));
+            ListenThread.IsBackground = true;
+            ListenThread.Start();
+        }
+
+        private void ListenLoop()
+        {
+            try
+            {
+                var endpoint = new IPEndPoint(IPAddress.Any, ListenPort);
+                while (true)
+                {
+                    var messageBytes = Client.Receive(ref endpoint);
+                    var message = GetMessageFromBytes(messageBytes);
+                    if (OnMessageReceived != null)
+                    {
+                        OnMessageReceived(this, message);
+                    }
+                }
+            }
+            catch (SocketException e)
+            {
+                RaiseError(e.Message);
+            }
+        }
+
+        private TMessage GetMessageFromBytes(byte[] messageBytes)
+        {
+            using (var stream = new MemoryStream(messageBytes))
+            {
+                return (TMessage)Formatter.Deserialize(stream);
+            }
+        }
+
+        private void CleanupClient()
+        {
+            try
+            {
+                Client.Close();
+            }
+            catch (SocketException e)
+            {
+                RaiseError(e.Message);
+            }
+            finally
+            {
+                Client = null;
+            }
+        }
+
+        private void CleanupThread()
+        {
+            try
+            {
+                if (!ListenThread.Join(800))
+                {
+                    ListenThread.Abort();
+                }
+            }
+            catch (Exception e)
+            {
+                RaiseError(e.Message);
+            }
+            finally
+            {
+                ListenThread = null;
+            }
+        }
+
+        public void StopListening()
+        {
+            if (Client != null)
+            {
+                CleanupClient();
+            }
+            else
+            {
+                RaiseError("Attempted to shutdown an inactive client");
+            }
+        }
+
+        private void RaiseError(string error)
+        {
+            if (OnNetworkError != null)
+            {
+                OnNetworkError(this, error);
+            }
+        }
+    }
+}
--- SMT.Networking/Udp/SimpleUdpMessenger.cs
+++ SMT.Networking/Udp/SimpleUdpMessenger.cs
@@ -15,151 +15,208 @@ namespace SMT.Networking.Udp
     public class SimpleUdpMessenger<TMessage> : ISimpleMessenger<TMessage>
         where TMessage : class
     {
-        public event EventHandler<TMessage> OnMessageReceived;
+        private SimpleUdpMessageListener<TMessage> Listener;
+        private SimpleUdpMessageSender<TMessage> Messenger;
 
-        private Thread ReceiveThread;
-        private UdpClient Client;
-        private bool IsConnected;
-        private BinaryFormatter Formatter;
+        private string Host;
+        private int Port;
 
-        private IPEndPoint RemoteEndPoint;
+        private bool Connected;
 
         public SimpleUdpMessenger()
         {
-            Client = null;
-            ReceiveThread = null;
-            Formatter = new BinaryFormatter();
+            Connected = false;
+            Listener = new SimpleUdpMessageListener<TMessage>();
+            Messenger = new SimpleUdpMessageSender<TMessage>();
+
+            Listener.OnMessageReceived += Listener_OnMessageReceived;
         }
+
+        void Listener_OnMessageReceived(object sender, TMessage e)
+        {
+            if (OnMessageReceived != null)
+            {
+                OnMessageReceived(this, e);
+            }
+        }
+
+        //public event EventHandler<TMessage> OnMessageReceived;
+
+        //private Thread ReceiveThread;
+        //private UdpClient Client;
+        //private bool IsConnected;
+        //private BinaryFormatter Formatter;
+
+        //private IPEndPoint RemoteEndPoint;
+
+        //public SimpleUdpMessenger()
+        //{
+        //    Client = null;
+        //    ReceiveThread = null;
+        //    Formatter = new BinaryFormatter();
+        //}
+
+        //public void Connect(string host, int port)
+        //{
+        //    if (!IsConnected)
+        //    {
+        //        try
+        //        {
+        //            IsConnected = true;
+
+        //            Client = new UdpClient(port);
+
+        //            if (!string.IsNullOrEmpty(host))
+        //            {
+        //                var resolvedHost = ResolveHostname(host);
+        //                RemoteEndPoint = new IPEndPoint(resolvedHost, port);
+        //                Client.Connect(RemoteEndPoint);//really only serves as a filter
+        //            }
+        //            else
+        //            {
+        //                RemoteEndPoint = new IPEndPoint(IPAddress.Any, port);
+        //            }
+
+        //            ReceiveThread = new Thread(new ThreadStart(ReceiveLoop));
+        //            ReceiveThread.IsBackground = true;
+        //            ReceiveThread.Start();
+        //        }
+        //        catch (SocketException e)
+        //        {
+        //            CleanupEverything();
+
+        //            throw new ApplicationException("Couldn't connect", e);
+        //        }
+        //    }
+        //}
+
+        //private IPAddress ResolveHostname(string host)
+        //{
+        //    var addresses = Dns.GetHostAddresses(host);
+        //    if (!addresses.Any())
+        //    {
+        //        throw new SocketException();
+        //    }
+
+        //    return addresses[0];
+        //}
+
+        //private void ReceiveLoop()
+        //{
+        //    try
+        //    {
+        //        while (IsConnected)
+        //        {
+        //            var messageBytes = Client.Receive(ref RemoteEndPoint);
+
+        //            TMessage message = null;
+        //            if (typeof(TMessage) == typeof(string))
+        //            {
+        //                message = GetDeserializedMessageString(messageBytes) as TMessage;
+        //            }
+        //            else
+        //            {
+        //                message = GetDeserializedMessage(messageBytes);
+        //            }
+
+        //            if (OnMessageReceived != null)
+        //            {
+        //                OnMessageReceived(this, message);
+        //            }
+        //        }
+        //    }
+        //    catch (SocketException e)
+        //    { }
+        //    catch (ThreadAbortException e)
+        //    { }
+        //}
+
+        //public void Send(TMessage message)
+        //{
+        //    if (IsConnected)
+        //    {
+        //        var serializedMessageBytes = GetSerializedMessage(message);
+        //        Client.Send(serializedMessageBytes, serializedMessageBytes.Length);
+        //    }
+        //}
+
+        //public void Disconnect()
+        //{
+        //    if (IsConnected)
+        //    {
+        //        CleanupEverything();
+        //    }
+        //}
+
+        //private void CleanupEverything()
+        //{
+        //    IsConnected = false;
+
+        //    try
+        //    {
+        //        Client.Close();
+        //    }
+        //    catch { }
+        //    Client = null;
+
+        //    if (!ReceiveThread.Join(1000))
+        //    {
+        //        ReceiveThread.Abort();
+        //    }
+        //    ReceiveThread = null;
+        //}
+
+        //private byte[] GetSerializedMessage(TMessage message)
+        //{
+        //    using (var stream = new MemoryStream())
+        //    {
+        //        Formatter.Serialize(stream, message);
+        //        return stream.ToArray();
+        //    }
+        //}
+
+        //private string GetDeserializedMessageString(byte[] bytes)
+        //{
+        //    return ASCIIEncoding.ASCII.GetString(bytes);
+        //}
+
+        //private TMessage GetDeserializedMessage(byte[] bytes)
+        //{
+        //    using (var stream = new MemoryStream(bytes))
+        //    {
+        //        return (TMessage)Formatter.Deserialize(stream);
+        //    }
+        //}
+
+        public event EventHandler<TMessage> OnMessageReceived;
 
         public void Connect(string host, int port)
         {
-            if (!IsConnected)
+            if (Connected)
             {
-                try
-                {
-                    IsConnected = true;
-
-                    Client = new UdpClient(port);
-
-                    if (!string.IsNullOrEmpty(host))
-                    {
-                        var resolvedHost = ResolveHostname(host);
-                        RemoteEndPoint = new IPEndPoint(resolvedHost, port);
-                        Client.Connect(RemoteEndPoint);//really only serves as a filter
-                    }
-                    else
-                    {
-                        RemoteEndPoint = new IPEndPoint(IPAddress.Any, port);
-                    }
-
-                    ReceiveThread = new Thread(new ThreadStart(ReceiveLoop));
-                    ReceiveThread.IsBackground = true;
-                    ReceiveThread.Start();
-                }
-                catch (SocketException e)
-                {
-                    CleanupEverything();
-
-                    throw new ApplicationException("Couldn't connect", e);
-                }
-            }
-        }
-
-        private IPAddress ResolveHostname(string host)
-        {
-            var addresses = Dns.GetHostAddresses(host);
-            if (!addresses.Any())
-            {
-                throw new SocketException();
+                Disconnect();
             }
 
-            return addresses[0];
-        }
-
-        private void ReceiveLoop()
-        {
-            try
-            {
-                while (IsConnected)
-                {
-                    var messageBytes = Client.Receive(ref RemoteEndPoint);
-                    
-                    TMessage message = null;
-                    if (typeof(TMessage) == typeof(string))
-                    {
-                        message = GetDeserializedMessageString(messageBytes) as TMessage;
-                    }
-                    else
-                    {
-                        message = GetDeserializedMessage(messageBytes);
-                    }
-
-                    if (OnMessageReceived != null)
-                    {
-                        OnMessageReceived(this, message);
-                    }
-                }
-            }
-            catch (SocketException e)
-            { }
-            catch (ThreadAbortException e)
-            { }
+            Listener.StartListening(port);
+            Host = host;
+            Port = port;
         }
 
         public void Send(TMessage message)
         {
-            if (IsConnected)
+            if (Connected)
             {
-                var serializedMessageBytes = GetSerializedMessage(message);
-                Client.Send(serializedMessageBytes, serializedMessageBytes.Length);
+                Messenger.SendMessage(Host, Port, message);
             }
         }
 
         public void Disconnect()
         {
-            if (IsConnected)
+            if (Connected)
             {
-                CleanupEverything();
-            }
-        }
-
-        private void CleanupEverything()
-        {
-            IsConnected = false;
-
-            try
-            {
-                Client.Close();
-            }
-            catch { }
-            Client = null;
-
-            if (!ReceiveThread.Join(1000))
-            {
-                ReceiveThread.Abort();
-            }
-            ReceiveThread = null;
-        }
-
-        private byte[] GetSerializedMessage(TMessage message)
-        {
-            using (var stream = new MemoryStream())
-            {
-                Formatter.Serialize(stream, message);
-                return stream.ToArray();
-            }
-        }
-
-        private string GetDeserializedMessageString(byte[] bytes)
-        {
-            return ASCIIEncoding.ASCII.GetString(bytes);
-        }
-
-        private TMessage GetDeserializedMessage(byte[] bytes)
-        {
-            using (var stream = new MemoryStream(bytes))
-            {
-                return (TMessage)Formatter.Deserialize(stream);
+                Listener.StopListening();
+                Host = null;
+                Port = 0;
             }
         }
     }
