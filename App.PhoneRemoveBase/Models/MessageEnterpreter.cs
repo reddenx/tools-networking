0ca12c263af6a29347bf5ae7857c17bf72d37238
SM_D <seanmarek@gmail.com>
2015-05-15 18:37:11 -0700

    mouse events, half donw

---
+++ App.PhoneRemoveBase/Models/MessageEnterpreter.cs
@@ -0,0 +1,54 @@
+﻿using SMT.Utilities.InputEvents.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.PhoneRemoveBase.Models
+{
+    class MessageEnterpreter
+    {
+        private readonly IKeyboardEventRunner Runner;
+
+        public MessageEnterpreter(IKeyboardEventRunner runner)
+        {
+            Runner = runner;
+        }
+
+        public void HandleMessage(string message)
+        {
+            message.Split(':');
+        }
+    }
+
+    static class MessageCommands
+    {
+        public const string LeftClick = "LCLICK";
+        public const string RightClick = "RCLICK";
+        public const string LeftDoubleClick = "LDBLLCICK";
+        public const string MoveMouse = "MOVE";
+        public const string StartLeftDrag = "LDRAG+";
+        public const string StopLeftDrag = "LDRAG-";
+    }
+
+    static class HardwareCommands
+    {
+        
+    }
+
+
+    /* message protocol
+     * 
+     * [COMMAND:DATA,DATA,DATA,DATA...]
+     * can only be one command per packet
+     * can only be 256 char long
+     * 
+     * commands include
+     * LCLICK
+     * RCLICK
+     * LDBLCLICK
+     * RDBLCLICK
+     * 
+     */
+}
---
+++ SMT.Utilities.InputEvents/HardwareEvents/MouseEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMT.Utilities.InputEvents.HardwareEvents
+{
+    public class MouseEventArgs
+    {
+        public readonly uint DiffX;
+        public readonly uint DiffY;
+        public readonly uint Flags;
+        public readonly uint Data;
+
+        public MouseEventArgs(uint diffX, uint diffY, uint flags, uint data)
+        {
+            this.DiffX = diffX;
+            this.DiffY = diffY;
+            this.Flags = flags;
+            this.Data = data;
+        }
+
+        public static MouseEventArgs LeftDown();
+        public static MouseEventArgs LeftUp();
+        public static MouseEventArgs RightDown();
+        public static MouseEventArgs RightUp();
+        public static MouseEventArgs ScrollUp();
+        public static MouseEventArgs ScrollDown();        
+        public static MouseEventArgs Move(
+    }
+}
