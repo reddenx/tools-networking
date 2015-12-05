0b2cad0521b632a85fa72de5d978e9e5d86cb2cd
SM_D <seanmarek@gmail.com>
2015-12-04 23:10:42 -0800

    little project cleanup

--- SMT.Utilities.Sql/Interfaces/ISqlQuerier.cs
+++ SMT.Utilities.Sql/Interfaces/ISqlQuerier.cs
@@ -12,7 +12,7 @@ namespace SMT.Utilities.Sql.Interfaces
 
     public interface ISqlQuerier
     {
-        T[] ExecuteReader<T>(BuildObjectFromReader<T> getObjectFromRecord, string sql, IDbDataParameter[] parameters);
+        T[] ExecuteReader<T>(string sql, IDbDataParameter[] parameters, BuildObjectFromReader<T> getObjectFromRecord);
         int InsertAndGetIdentity(string sql, IDbDataParameter[] parameters);
         int ExecuteNonQuery(string sql, IDbDataParameter[] parameters);
 
