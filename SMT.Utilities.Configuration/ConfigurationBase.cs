b28130fc6ba5dea7553beeec13f73bcf12d2baf2
SM_D <seanmarek@gmail.com>
2016-09-22 21:03:16 -0700

    simple and quick cleanup for deployment

--- SMT.Utilities.Configuration/ConfigurationBase.cs
+++ SMT.Utilities.Configuration/ConfigurationBase.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SMT.Utilities.Configuration
 {
