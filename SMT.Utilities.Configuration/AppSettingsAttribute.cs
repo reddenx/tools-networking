ed4e1ba7e120efd9279452886aa2d6d5ec363034
skip <sean@altsrc.net>
2016-10-05 11:47:23 -0700

    configuration manager supports defaults and overrides on fail

--- SMT.Utilities.Configuration/AppSettingsAttribute.cs
+++ SMT.Utilities.Configuration/AppSettingsAttribute.cs
@@ -11,13 +11,25 @@ namespace SMT.Utilities.Configuration
         /// </summary>
         public string Name { get; private set; }
 
-        /// <summary>
-        /// Marks a field to be populated by an entry in configuration.appsetings from a web or app config
-        /// </summary>
-        /// <param name="name">The name of the entry in appsettings</param>
-        public AppSettingsAttribute(string name)
+		/// <summary>
+		/// Flag indicating if the application will throw an error if this entry is missing from the config
+		/// </summary>
+		public bool ErrorIfMissing { get; private set; }
+
+		/// <summary>
+		/// Entry to default to if the entry is missing from the config
+		/// </summary>
+		public string DefaultValue { get; private set; }
+
+		/// <summary>
+		/// Marks a field to be populated by an entry in configuration.appsetings from a web or app config
+		/// </summary>
+		/// <param name="name">The name of the entry in appsettings</param>
+		public AppSettingsAttribute(string name, string defaultValue = null, bool errorOnMissing = true)
         {
-            Name = name;
+			DefaultValue = defaultValue;
+			ErrorIfMissing = errorOnMissing;
+			Name = name;
         }
     }
 }
