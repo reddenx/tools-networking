81a4989f4510dd58c3c566823b248991689b3d27
sm_d3po <seanmarek@gmail.com>
2017-09-09 19:00:39 -0700

    type generation boxing problem solved, unit tests added

--- SMT.Utilities.Reflection/TypeFactory.cs
+++ SMT.Utilities.Reflection/TypeFactory.cs
@@ -1,178 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SMT.Utilities.Reflection
 {
-    public class TypeFactory
+    public static class TypeFactory
     {
-        private TypeBuilder TypeBuilderInstance;
-
-        public TypeFactory(string className, Type parentType, Type[] interfaces)
+        //what I want out of this
+        //a masked type to hand out and
+        //an interceptor interface to handle calls
+        public static GeneratedTypeResult<MaskedType> BuildType<MaskedType>()
         {
-            var assemblyName = Assembly.GetCallingAssembly().GetName();
-            var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
-            var moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName.Name);
+            var inputType = typeof(MaskedType);
 
-            TypeBuilderInstance = moduleBuilder.DefineType(className,
-                TypeAttributes.Public |
-                TypeAttributes.Class |
-                TypeAttributes.AutoClass |
-                TypeAttributes.AnsiClass |
-                TypeAttributes.BeforeFieldInit |
-                TypeAttributes.AutoLayout,
-                parentType);
+            //inquiry revealed this as necessary for this implementation
+            if (!inputType.IsInterface)
+                throw new NotImplementedException();
 
-            if (interfaces != null && interfaces.Any())
+            //get and define a home for this new type
+            var assemblyBuilder = Thread.GetDomain().DefineDynamicAssembly(
+                name: Assembly.GetAssembly(typeof(MaskedType)).GetName(),
+                access: AssemblyBuilderAccess.Run);
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule($"TypeInterception.{inputType.Name}");
+            var typeBuilder = moduleBuilder.DefineType($"TypeInterception.{inputType.Name}.{Guid.NewGuid()}");
+
+            //setup the heirarchy, parent is the target type, base type is the interceptor
+            var baseType = typeof(BaseInterceptor);
+            typeBuilder.AddInterfaceImplementation(inputType);
+            typeBuilder.SetParent(baseType);
+
+            var constructorBuilder = typeBuilder.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, new Type[] { typeof(Type) });
+            var ctorIlGen = constructorBuilder.GetILGenerator();
+
+            //build constructor
+            var baseConstructor = baseType.GetConstructors()[0];
+            ctorIlGen.Emit(OpCodes.Ldarg, 0); //load this (this keyword is always arg0 in an instanced object)
+            ctorIlGen.Emit(OpCodes.Ldarg, 1); //load argument 1 (Type targetType)
+            ctorIlGen.Emit(OpCodes.Call, baseConstructor); //make the call
+            ctorIlGen.Emit(OpCodes.Ret); //return
+
+            //get the methods we want to target in the base type
+            var methodWithReturn = baseType.GetMethod("Func");
+            var methodWithoutReturn = baseType.GetMethod("Action");
+
+            //build up methods
+            var methods = inputType.GetMethods();
+            for (int j = 0; j < methods.Length; ++j)
             {
-                foreach (var interfaceDefinition in interfaces)
+                var method = methods[j];
+
+                //define a new method based on the target method and prime the ilgeneration
+                var newMethod = typeBuilder.DefineMethod(method.Name, MethodAttributes.Public | MethodAttributes.Virtual, method.CallingConvention, method.ReturnType, method.GetParameters().Select(parameter => parameter.ParameterType).ToArray());
+                var ilGenerator = newMethod.GetILGenerator();
+                var inputParameters = method.GetParameters();
+
+
+                //the purpose of this IL is to take the argument list and turn it into an object array to be consumed by the interceptor's Action and Func methods;
+                var array = ilGenerator.DeclareLocal(typeof(object[])); //declare a local array of type object
+
+                ilGenerator.Emit(OpCodes.Ldc_I4, inputParameters.Length); //load arbitrary int of array length
+                ilGenerator.Emit(OpCodes.Newarr, typeof(object)); //new up array of type object, consumes the int
+                ilGenerator.Emit(OpCodes.Stloc, array); //set this pops the new array and sets it into the local variable declared earlier
+
+                for (int i = 0; i < inputParameters.Length; ++i)
                 {
-                    TypeBuilderInstance.AddInterfaceImplementation(interfaceDefinition);
+                    ilGenerator.Emit(OpCodes.Ldloc, array); //loads the array
+                    ilGenerator.Emit(OpCodes.Ldc_I4, i); //loads an arbitrary int (the index for the next command)
+                    ilGenerator.Emit(OpCodes.Ldarg, i + 1); //loads an input argument
+
+                    //if the parameter requires boxing, box it (value types and enums)
+                    var parameterType = inputParameters[i].ParameterType;
+                    if (parameterType.IsValueType || parameterType.IsEnum)
+                    {
+                        ilGenerator.Emit(OpCodes.Box, parameterType);
+                    }
+
+                    ilGenerator.Emit(OpCodes.Stelem_Ref); //move object into target index, pops args1-n and 'this' (arg0)
                 }
-            }
-        }
 
-        public void OverrideMethod<MethodDefinition>(string methodName, Type overridingType, string methodToOverride, Expression<MethodDefinition> expressionBody)
-        {
-            var method = CompileExpressionToMethod(methodName, expressionBody);
+                //make method call
+                ilGenerator.Emit(OpCodes.Ldarg_0); //load this
+                ilGenerator.Emit(OpCodes.Ldloc, array); //load the array
+                ilGenerator.Emit(OpCodes.Ldc_I4, j); //load the target method index
 
-            //var methodBuilderStatic = TypeBuilderInstance.DefineMethod("_" + methodName,
-            //    MethodAttributes.Public |
-            //    MethodAttributes.Static);
-            //expressionBody.CompileToMethod(methodBuilderStatic);
-            
-            //var methodBuilder = TypeBuilderInstance.DefineMethod(methodName, MethodAttributes.Public | MethodAttributes.Virtual);
+                if (method.ReturnType != typeof(void))
+                {
+                    ilGenerator.Emit(OpCodes.Callvirt, methodWithReturn); //consumes the object array and index int, targets the Func so we get a return value
 
-            TypeBuilderInstance.DefineMethodOverride(method, overridingType.GetMethod(methodToOverride));
+                    //if the return type requires boxing, unbox it and reload it's value to the stack before returning it
+                    if (method.ReturnType.IsValueType || method.ReturnType.IsEnum)
+                    {
+                        ilGenerator.Emit(OpCodes.Unbox, method.ReturnType);
+                        ilGenerator.Emit(OpCodes.Ldobj, method.ReturnType);
+                    }
+                }
+                else
+                {
+                    ilGenerator.Emit(OpCodes.Callvirt, methodWithoutReturn); //consumes the object array and index int, targets the Action so we don't get a return value
+                }
 
-            //methodBuilder.SetReturnType(expressionBody.ReturnType);
-            //
-            //var ilGenerator = methodBuilder.GetILGenerator();
-            //
-            //var types = expressionBody.Parameters.Select(p => p.Type).ToArray();
-            //methodBuilder.SetParameters(types);
-            //
-            //for (Int32 i = 1; i <= types.Length; ++i)
-            //{
-            //    ilGenerator.Emit(OpCodes.Ldarg, i);
-            //}
-            //
-            //ilGenerator.EmitCall(OpCodes.Call, methodBuilderStatic, null);
-            //ilGenerator.Emit(OpCodes.Ret);
-        }
-
-        public void AppendMethod<MethodDefinition>(string methodName, Expression<MethodDefinition> expressionBody)
-        {
-            var methodBuilder = CompileExpressionToMethod(methodName, expressionBody);
-
-            //var methodInfo = (lambda as Delegate);
-            //if (methodInfo == null)
-            //{
-            //    throw new ArgumentException("lambda must be a delegate", "lambda");
-            //}
-            //
-            //var returnType = methodInfo.Method.ReturnType;
-            //var parameters = methodInfo.Method.GetParameters().Select(p => p.ParameterType).ToArray();
-            //
-            //var methodBuilder = TypeBuilderInstance.DefineMethod(methodName, 
-            //    MethodAttributes.Public | MethodAttributes.Static,
-            //    returnType,
-            //    parameters);
-            //
-            //var il = methodInfo.Method.GetMethodBody().GetILAsByteArray();
-            //methodBuilder.CreateMethodBody(il, il.Length);
-            //
-            //var ilGenerator = methodBuilder.GetILGenerator();
-
-
-
-            //turn expression into private static method
-            //var methodBuilderStatic = TypeBuilderInstance.DefineMethod("_" + methodName,
-            //    MethodAttributes.Public |
-            //    MethodAttributes.Static);
-            //expressionBody.CompileToMethod(methodBuilderStatic);
-            
-            //var methodBuilder = TypeBuilderInstance.DefineMethod(methodName,
-            //    MethodAttributes.Public);
-            
-            
-            //methodBuilder.SetReturnType(expressionBody.ReturnType);
-            
-            //var ilGenerator = methodBuilder.GetILGenerator();
-
-            //var types = expressionBody.Parameters.Select(p => p.Type).ToArray();
-            //methodBuilder.SetParameters(types);
-
-            //for (Int32 i = 1; i <= types.Length; ++i)
-            //{
-            //    ilGenerator.Emit(OpCodes.Ldarg, i);
-            //}
-
-            //ilGenerator.EmitCall(OpCodes.Call, methodBuilderStatic, null);
-            //ilGenerator.Emit(OpCodes.Ret);
-
-
-            //var ilGen = methodBuilder.GetILGenerator();
-            //ilGen.Emit(
-
-
-
-
-            //var methodBuilder = typeBuilder.DefineMethod(
-            //    method.Name,
-            //    MethodAttributes.Virtual |
-            //    MethodAttributes.Public);
-
-            //var ilGenerator = methodBuilder.GetILGenerator();
-            //ilGenerator.Emit(OpCodes.pop
-
-
-
-
-
-
-            //TODO-SM only one method override currently supported
-            //var interfaceMethod = interfaces.GetMethod(method.Name);
-            //typeBuilder.DefineMethodOverride(method, interfaceMethod);
-        }
-
-        private MethodBuilder CompileExpressionToMethod<T>(string methodName, Expression<T> expressionBody)
-        {
-            var methodBuilderStatic = TypeBuilderInstance.DefineMethod("_" + methodName,
-                MethodAttributes.Public |
-                MethodAttributes.Static);
-            expressionBody.CompileToMethod(methodBuilderStatic);
-
-            var methodBuilder = TypeBuilderInstance.DefineMethod(methodName,
-                MethodAttributes.Public | MethodAttributes.Virtual);
-
-
-            methodBuilder.SetReturnType(expressionBody.ReturnType);
-
-            var ilGenerator = methodBuilder.GetILGenerator();
-
-            var types = expressionBody.Parameters.Select(p => p.Type).ToArray();
-            methodBuilder.SetParameters(types);
-
-            for (Int32 i = 1; i <= types.Length; ++i)
-            {
-                ilGenerator.Emit(OpCodes.Ldarg, i);
+                ilGenerator.Emit(OpCodes.Ret); //finally return, and this method IL is complete
             }
 
-            ilGenerator.EmitCall(OpCodes.Call, methodBuilderStatic, null);
-            ilGenerator.Emit(OpCodes.Ret);
+            var generatedType = typeBuilder.CreateType();
+            var instance = Activator.CreateInstance(generatedType, inputType);
 
-            return methodBuilder;
-        }
+            return new GeneratedTypeResult<MaskedType>(
+                maskedType: (MaskedType)instance,
+                interceptor: (Interceptor)instance,
+                generatedType: generatedType);
+        }   
+    }
 
-        public Type Generate()
-        {
-            return this.TypeBuilderInstance.CreateType();
-        }
+    public delegate object InterceptMethod(object[] parameters);
+    public interface Interceptor
+    {
+        void SetImplementation(string methodName, Type[] parameters, InterceptMethod methodBody);
+        void SetImplementation<T>(string methodName, T implementation);
     }
 }
