07cd093f82d647cac1893e2a75c98457a4e192b5
sm_d3po <seanmarek@gmail.com>
2019-07-21 21:02:40 -0700

    type cache added due to reflection custom attribute inefficiencies.

--- SMT.Utilities.Configuration/ConfigurationBase.cs
+++ SMT.Utilities.Configuration/ConfigurationBase.cs
@@ -6,60 +6,91 @@ using System.Reflection;
 
 namespace SMT.Utilities.Configuration
 {
-	public abstract class ConfigurationBase
-	{
-		protected ConfigurationBase()
-		{
+    public abstract class ConfigurationBase
+    {
+        private static readonly Dictionary<Type, ConfigurationFieldInfo[]> _cache = new Dictionary<Type, ConfigurationFieldInfo[]>();
+
+        private class ConfigurationFieldInfo
+        {
+            public FieldInfo field;
+            public PropertyInfo prop;
+            public AppSettingsAttribute appsetting;
+            public ConnectionStringAttribute connection;
+        }
+
+        protected ConfigurationBase()
+        {
             //get all appsettings fields
             var configurationErrorMessages = new List<string>();
 
-            //should DRY this up eventually, too bad there's a 4x permutation on field/property type and appsetting/connection strings
-            var appSettingsFields = this.GetType().GetFields().Where(f => f.GetCustomAttribute<AppSettingsAttribute>() != null);
-            foreach (var appSettingsField in appSettingsFields)
+            var type = this.GetType();
+            ConfigurationFieldInfo[] data;
+
+            if (_cache.ContainsKey(type))
             {
-                var attr = appSettingsField.GetCustomAttribute<AppSettingsAttribute>();
-				if (!TrySetValueFromConfig(attr.Name, attr.ErrorIfMissing, attr.DefaultValue, (s) => ConfigurationManager.AppSettings[s], appSettingsField.FieldType, appSettingsField.SetValue))
-				{
-                    configurationErrorMessages.Add(string.Format("AppSetting:{0}", attr.Name));
-				}
+                data = _cache[type];
+            }
+            else
+            {
+                data = type.GetFields().Select(f => new ConfigurationFieldInfo
+                {
+                    field = f,
+                    appsetting = f.GetCustomAttribute<AppSettingsAttribute>(),
+                    connection = f.GetCustomAttribute<ConnectionStringAttribute>(),
+                }).Concat(type.GetProperties().Select(f => new ConfigurationFieldInfo
+                {
+                    prop = f,
+                    appsetting = f.GetCustomAttribute<AppSettingsAttribute>(),
+                    connection = f.GetCustomAttribute<ConnectionStringAttribute>(),
+                })).ToArray();
+                _cache[type] = data;
             }
 
-            var appSettingsProperties = this.GetType().GetProperties().Where(p => p.GetCustomAttribute<AppSettingsAttribute>() != null);
-            foreach (var appSettingsProperty in appSettingsProperties)
+            //fields
+            foreach (var item in data)
             {
-				var attr = appSettingsProperty.GetCustomAttribute<AppSettingsAttribute>();
-				if (!TrySetValueFromConfig(attr.Name, attr.ErrorIfMissing, attr.DefaultValue, (s) => ConfigurationManager.AppSettings[s], appSettingsProperty.PropertyType, appSettingsProperty.SetValue))
-				{
-					configurationErrorMessages.Add(string.Format("AppSetting:{0}", attr.Name));
-				}
+                if (item.field != null)
+                {
+                    if (item.appsetting != null)
+                    {
+                        if (!TrySetValueFromConfig(item.appsetting.Name, item.appsetting.ErrorIfMissing, item.appsetting.DefaultValue, (s) => ConfigurationManager.AppSettings[s], item.field.FieldType, item.field.SetValue))
+                        {
+                            configurationErrorMessages.Add(string.Format("AppSetting:{0}", item.appsetting.Name));
+                        }
+                    }
+                    if (item.connection != null)
+                    {
+                        if (!TrySetValueFromConfig(item.connection.Name, item.connection.ErrorIfMissing, item.connection.DefaultValue, (s) => ConfigurationManager.ConnectionStrings[s].ConnectionString, item.field.FieldType, item.field.SetValue))
+                        {
+                            configurationErrorMessages.Add(string.Format("ConnectionString:{0}", item.connection.Name));
+                        }
+                    }
+                }
+                if (item.prop != null)
+                {
+                    if (item.appsetting != null)
+                    {
+                        if (!TrySetValueFromConfig(item.appsetting.Name, item.appsetting.ErrorIfMissing, item.appsetting.DefaultValue, (s) => ConfigurationManager.AppSettings[s], item.prop.PropertyType, item.prop.SetValue))
+                        {
+                            configurationErrorMessages.Add(string.Format("AppSetting:{0}", item.appsetting.Name));
+                        }
+                    }
+                    if (item.connection != null)
+                    {
+                        if (!TrySetValueFromConfig(item.connection.Name, item.connection.ErrorIfMissing, item.connection.DefaultValue, (s) => ConfigurationManager.ConnectionStrings[s].ConnectionString, item.prop.PropertyType, item.prop.SetValue))
+                        {
+                            configurationErrorMessages.Add(string.Format("ConnectionString:{0}", item.connection.Name));
+                        }
+                    }
+                }
             }
-
-            var connectionStringFields = this.GetType().GetFields().Where(f => f.GetCustomAttribute<ConnectionStringAttribute>() != null);
-            foreach (var connectionStringField in connectionStringFields)
-            {
-                var attr = connectionStringField.GetCustomAttribute<ConnectionStringAttribute>();
-				if (!TrySetValueFromConfig(attr.Name, attr.ErrorIfMissing, attr.DefaultValue, (s) => ConfigurationManager.ConnectionStrings[s].ConnectionString, connectionStringField.FieldType, connectionStringField.SetValue))
-				{
-					configurationErrorMessages.Add(string.Format("ConnectionString:{0}", attr.Name));
-				}
-			}
-
-            var connectionStringProperties = this.GetType().GetProperties().Where(p => p.GetCustomAttribute<ConnectionStringAttribute>() != null);
-            foreach(var connectionStringProperty in connectionStringProperties)
-            {
-                var attr = connectionStringProperty.GetCustomAttribute<ConnectionStringAttribute>();
-				if (!TrySetValueFromConfig(attr.Name, attr.ErrorIfMissing, attr.DefaultValue, (s) => ConfigurationManager.ConnectionStrings[s].ConnectionString, connectionStringProperty.PropertyType, connectionStringProperty.SetValue))
-				{
-					configurationErrorMessages.Add(string.Format("ConnectionString:{0}", attr.Name));
-				}
-			}
 
             if (configurationErrorMessages.Any())
             {
                 var compositeMessage = string.Format("Missing Configuration Value(s): {0}", string.Join(", ", configurationErrorMessages));
                 throw new ConfigurationErrorsException(compositeMessage);
             }
-		}
+        }
 
         private object GetConvertedValue(string stringValue, Type type)
         {
@@ -73,20 +104,20 @@ namespace SMT.Utilities.Configuration
             }
         }
 
-		private bool TrySetValueFromConfig(string name, bool failIfMissing, string defaultValue, Func<string,string> getFromConfig, Type destinationType, Action<object, object> setValue)
-		{
-			var value = getFromConfig(name);
-			var convertedValue = GetConvertedValue(value ?? defaultValue, destinationType);
+        private bool TrySetValueFromConfig(string name, bool failIfMissing, string defaultValue, Func<string, string> getFromConfig, Type destinationType, Action<object, object> setValue)
+        {
+            var value = getFromConfig(name);
+            var convertedValue = GetConvertedValue(value ?? defaultValue, destinationType);
 
-			if (convertedValue != null)
-			{
-				setValue(this, convertedValue);
-			}
-			else if (failIfMissing)
-			{
-				return false;
-			}
-			return true;
-		}
-	}
+            if (convertedValue != null)
+            {
+                setValue(this, convertedValue);
+            }
+            else if (failIfMissing)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
 }
